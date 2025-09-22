dfb3fda23831743421ab4491bbd3342fee823efb
Harshit_Parmar <parmarharshit441@gmail.com>
2025-09-22 14:42:57 +0530

    set withCredentials true

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "http://localhost:4000/api/v1",
 });
+axiosInstance.defaults.withCredentials = true;
