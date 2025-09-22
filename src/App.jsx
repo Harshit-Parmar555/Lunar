8695844b0fdf376b8e9063fe6ea4f52e370989eb
Harshit_Parmar <parmarharshit441@gmail.com>
2025-09-22 15:01:00 +0530

    setup resizable main layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import AuthCallbackPage from "./pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layouts/MainLayout";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
       <Route
         path="/sso-callback"
         element={
@@ -16,6 +16,9 @@ const App = () => {
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+      </Route>
     </Routes>
   );
 };
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,34 @@
+import {
+  ResizablePanel,
+  ResizableHandle,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  const isMobile = false;
+  return (
+    <div className="h-screen bg-black text-white flex flex-col gap-2">
+      <ResizablePanelGroup
+        direction="horizontal"
+        className="flex flex-1 h-full overflow-hidden p-2"
+      >
+        <ResizablePanel
+          defaultSize={isMobile ? 0 : 20}
+          minSize={isMobile ? 0 : 15}
+          maxSize={isMobile ? 0 : 30}
+          className="border-r border-zinc-800 pr-2"
+        >
+          <h1>left sidebar</h1>
+        </ResizablePanel>
+
+        <ResizableHandle />
+        <ResizablePanel defaultSize={80} className="overflow-auto">
+          <Outlet />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  );
+};
+
+export default MainLayout;
