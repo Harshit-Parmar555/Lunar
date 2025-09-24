75c5abfaaaee6224ebbed443f58a2f7d8a1b18ac
Harshit_Parmar <parmarharshit441@gmail.com>
2025-09-24 23:43:01 +0530

    modify authProvider according to new flow

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,10 +1,12 @@
-import { useState, useEffect, use } from "react";
-import { useAuth, useUser } from "@clerk/clerk-react";
-import { Loader } from "lucide-react";
 import { axiosInstance } from "@/lib/axios";
 import useAuthStore from "@/stores/useAuthStore";
+import { useAuth } from "@clerk/clerk-react";
+import { Loader } from "lucide-react";
+import { useState, useEffect } from "react";
 
-const updateAuthToken = async (token) => {
+
+
+const updateAuthToken = (token) => {
   if (token) {
     axiosInstance.defaults.headers.common["Authorization"] = `Bearer ${token}`;
   } else {
@@ -13,11 +15,9 @@ const updateAuthToken = async (token) => {
 };
 
 const AuthProvider = ({ children }) => {
-  const [loading, setloading] = useState(true);
-  const { getToken, userId, isLoaded: authLoaded, isSignedIn } = useAuth();
-  const { user, isLoaded: userLoaded } = useUser();
-  const { initializeAuth, resetAuth } = useAuthStore();
-  const { isAdmin, checkAdmin } = useAuthStore();
+  const { getToken, userId } = useAuth();
+  const [loading, setLoading] = useState(true);
+  const {checkAdmin} = useAuthStore();
 
   useEffect(() => {
     const authenticate = async () => {
@@ -28,28 +28,17 @@ const AuthProvider = ({ children }) => {
           await checkAdmin();
         }
       } catch (error) {
-        console.log("Error in AuthProvider", error);
-      } finally {
-        setloading(false);
+        updateAuthToken(null);
+        console.error("Error during authentication:", error);
+      }finally{
+        setLoading(false);
       }
     };
 
-    if (authLoaded) {
-      authenticate();
-    }
-  }, [getToken, userId, authLoaded]);
+    authenticate();
+  }, [getToken,userId,checkAdmin]);
 
-  useEffect(() => {
-    if (authLoaded && userLoaded) {
-      if (isSignedIn && user) {
-        initializeAuth(user, true);
-      } else {
-        resetAuth();
-      }
-    }
-  }, [user, authLoaded, userLoaded, isSignedIn, initializeAuth, resetAuth]);
-
-  if (loading || !authLoaded || !userLoaded) {
+  if (loading) {
     return (
       <div className="w-full h-screen flex items-center justify-center">
         <Loader className="text-emerald-600 animate-spin" />
