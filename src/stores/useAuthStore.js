6d70d411d298b4681ed488557b8b6acfe8706afd
Harshit_Parmar <parmarharshit441@gmail.com>
2025-09-26 20:17:11 +0530

    implement useAuthStore

--- src/stores/useAuthStore.js
+++ src/stores/useAuthStore.js
@@ -1,26 +1,59 @@
-import { axiosInstance } from "@/lib/axios";
 import { create } from "zustand";
+import { auth, provider, signInWithPopup } from "@/lib/firebaseConfig";
+import { axiosInstance } from "@/lib/axios";
+import toast from "react-hot-toast";
 
-const useAuthStore = create((set, get) => ({
-  // State
-  isAdmin: false,
-  isLoading: false,
-  error: null,
+const useAuthStore = create((set) => ({
+  user: null,
+  isAuthenticated: false,
+  signingIn: false,
+  loggingOut: false,
+  checkingAuth: true,
+  isAdmin : false,
 
-  checkAdmin: async () => {
-    set({ isLoading: true, error: null });
+  signup: async () => {
     try {
-      const response = await axiosInstance.get("/admin/check");
-      set({ isAdmin: response.data.admin });
+      set({ signingIn: true });
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      const tokenId = await user.getIdToken();
+      // signup the user in your backend
+      const response = await axiosInstance.post("/auth/signup", {tokenId });
+      toast.success(response.data.message || "Signup successful");
+      set({ user: response.data.user, isAuthenticated: true });
     } catch (error) {
-      set({ isAdmin: false, error: error.response.data.message });
+      toast.error(error.response?.data?.message || "Signup failed");
     } finally {
-      set({ isLoading: false });
+      set({ signingIn: false });
     }
   },
 
-  reset: () => {
-    set({ isAdmin: false, isLoading: false, error: null });
+  logout: async () => {
+    try {
+      set({ loggingOut: true });
+      const response = await axiosInstance.post("/auth/logout");
+      toast.success(response.data.message || "Logged out successfully");
+      set({ user: null, isAuthenticated: false });
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Logout failed");
+    } finally {
+      set({ loggingOut: false });
+    }
+  },
+
+  checkAuth: async () => {
+    try {
+      const response = await axiosInstance.get("/auth/check");
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isAdmin : response.data.isAdmin
+      });
+    } catch (error) {
+      console.error("Authentication check failed:", error);
+    } finally {
+      set({ checkingAuth: false });
+    }
   },
 }));
 
