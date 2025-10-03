b50bcc9f79c34f70a6fe697f7e8d9bfb4b11b338
Harshit_Parmar <parmarharshit441@gmail.com>
2025-10-03 22:04:43 +0530

    add dynamic view port hieght

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -29,7 +29,7 @@ const MainLayout = () => {
 
   if (isMobile === null) {
     return (
-      <div className="w-full h-screen flex items-center justify-center bg-black">
+      <div className="w-dvh h-screen flex items-center justify-center bg-black">
         <Loader className="text-white animate-spin" />
       </div>
     );
